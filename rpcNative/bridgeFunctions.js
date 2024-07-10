954735d1c43a3eea31e219b25db0a2619fb54364
Sonal Bansal <33753404+sonal-bansal@users.noreply.github.com>
2024-07-10 12:51:08 +0530

    Add files via upload

---
+++ rpcNative/bridgeFunctions.js
@@ -0,0 +1,10 @@
+import { Sum, PrintLog, PrintLocation, Camera } from './utilities';
+
+const bridgeFunctions = {
+    'Sum': Sum,
+    'PrintLog': PrintLog,
+    'PrintLocation': PrintLocation,
+    'Camera': Camera,
+};
+
+export default bridgeFunctions;
